1b99aca6cfaa2912b525075ce13de3604bfde975
Jonibek <sjonibek@gmail.com>
2022-04-24 20:50:48 +0500

    Angular 13 contactApp the first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactManagerComponent } from './contact-manager/contact-manager.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/contacts/admin', pathMatch: 'full' },
+  { path: 'contacts/admin', component: ContactManagerComponent },
+  { path: 'contacts/add', component: AddContactComponent },
+  { path: 'contacts/edit/:contactId', component: EditContactComponent },
+  { path: 'contacts/view/:contactId', component: ViewContactComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
